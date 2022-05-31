19d2300222d1ca8de8ca414d34a0f8d45b2ed958
Rafael-Minnigaliev <rvfael@bk.ru>
2022-05-31 14:55:22 +0500

    правка-7

--- local/templates/taxi/header.php
+++ local/templates/taxi/header.php
@@ -29,20 +29,23 @@ use Bitrix\Main\Page\Asset;
             <img src="<?= SITE_TEMPLATE_PATH ?>/images/logo.svg" alt="">
         </div>
 		<? $APPLICATION->IncludeComponent(
-			'bitrix:menu',
-			'menu',
-			array(
-				'ALLOW_MULTI_SELECT' => 'N',
-				'CHILD_MENU_TYPE' => 'left',
-				'DELAY' => 'N',
-				'MAX_LEVEL' => '1',
-				'MENU_CACHE_GET_VARS' => array(0 => '',),
-				'MENU_CACHE_TIME' => '3600',
-				'MENU_CACHE_TYPE' => 'N',
-				'MENU_CACHE_USE_GROUPS' => 'Y',
-				'ROOT_MENU_TYPE' => 'top',
-				'USE_EXT' => 'N'
-			)
-		); ?>
+	"bitrix:menu", 
+	"menu", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "1",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "Y",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "top",
+		"USE_EXT" => "N",
+		"COMPONENT_TEMPLATE" => "menu"
+	),
+	false
+); ?>
     </div>
 </header>
