306d2edd5473c6e4d686cff7d5431135de51f613
Rafael-Minnigaliev <rvfael@bk.ru>
2022-05-31 11:39:47 +0500

    промежуточные правки

--- local/templates/taxi/components/bitrix/menu/menu/template.php
+++ local/templates/taxi/components/bitrix/menu/menu/template.php
@@ -1,4 +1,4 @@
-<?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+<?php if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 	die();
 } ?>
 
@@ -6,7 +6,7 @@
     <div class="header-menu">
         <ul>
 			<?php foreach ($arResult as $arItem): ?>
-                <li class="menu-item"><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+                <li class="menu-item"><a href="<?= $arItem['LINK'] ?>"><?= $arItem['TEXT'] ?></a></li>
 			<? endforeach ?>
         </ul>
         <a class="roundBtn" href="tel:+79991234569"><?= sprint_options_get('PHONE1') ?></a>
@@ -17,7 +17,7 @@
             <div class="wrapper">
                 <ul>
 					<?php foreach ($arResult as $arItem): ?>
-                        <li class="menu-item"><a href="<?= $arItem["LINK"] ?>"><?= $arItem["TEXT"] ?></a></li>
+                        <li class="menu-item"><a href="<?= $arItem['LINK'] ?>"><?= $arItem['TEXT'] ?></a></li>
 					<? endforeach ?>
                 </ul>
             </div>
